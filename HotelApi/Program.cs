45465e0b4e779f102cc719347b13d3f0c725eb99
jefy2110 <jaafacake2110@gmail.com>
2024-12-19 22:59:13 +0000

    cors setup for localhotst

--- HotelApi/Program.cs
+++ HotelApi/Program.cs
@@ -1,17 +1,29 @@
 var builder = WebApplication.CreateBuilder(args);
 
-
 builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:3000") 
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
 
 var app = builder.Build();
 
-
 if (app.Environment.IsDevelopment())
 {
-    app.UseDeveloperExceptionPage(); 
+    app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowFrontend");
+
+app.UseAuthorization();
 
 app.MapControllers();
 
